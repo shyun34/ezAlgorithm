591d06b4534c72c0413b874bcf5019b8c3c736bd
O-yujeong <yujeongoh09@gmail.com>
2021-01-31 23:53:12 +0900

    stack

--- OYJ/WEEK1/괄호의 값.py
+++ OYJ/WEEK1/괄호의 값.py
@@ -5,38 +5,40 @@
 # 입력값이 유효한지 체크
 def isValid(input):
 
-  res = True
+  
   if isinstance(input, list):
     bin = []
-    if len(input) % 2 != 0: res = False
-    if input[0] == ')' or input[0] == ']': res = False
+    if len(input) % 2 != 0: return False
+    if input[0] == ')' or input[0] == ']': return False
     for i in input:
       if i == '(' or i == '[': bin.append(i)
       elif i == ')':
-        if bin[-1] != '(' or len(bin) == 0: res = False
+        if bin[-1] != '(' or len(bin) == 0: return False
         else: bin.pop()
       elif i == ']':
-        if bin[-1] != '[' or len(bin) == 0: res = False
+        if bin[-1] != '[' or len(bin) == 0: return False
         else: bin.pop()
-  else: res = False
-  return res
+  else: return False
+  return True
 
 # 값 구하기
 def getValue(input):
-  a = []
-  b = []
+  calc = [0,0]
   stack = []
   val = 0
 
   if isValid(input):
     for i in input:
-      if i == '(' or i == '[':
+      if i == '(':
         stack.append(i)
+        calc[0] += 1
       elif i == ')':
+        val += 2
         
       elif i == ']':
 
   else: val = 0
+
 
   return val
 
