91e8e74622ed5dc972d30327b3c6bc7244983b00
unknown <bsj04155@naver.com>
2021-02-03 21:07:27 +0900

    승진 알고리즘 첫번째

---
+++ 백승진/P_01/P_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> arr = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(int.Parse(Console.ReadLine()));
+            }
+
+            arr.Sort();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr.ElementAt(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 백승진/P_01/P_04/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+
+            Stack<int> s = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if(arr[i].Contains("push"))
+                {
+                    s.Push(int.Parse(arr[i].Substring(arr[i].IndexOf(' ')+1)));
+                }
+                else if(arr[i].Contains("pop"))
+                {
+                    if(s.Count <= 0)
+                    {
+                        Console.WriteLine(-1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(s.Pop());
+                    }
+                }
+                else if(arr[i].Contains("size"))
+                {
+                    Console.WriteLine(s.Count);
+                }
+                else if(arr[i].Contains("empty"))
+                {
+                    if(s.Count <= 0)
+                    {
+                        Console.WriteLine(1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(0);
+                    }
+                }
+                else if(arr[i].Contains("top"))
+                {
+                    Console.WriteLine(s.Peek());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
